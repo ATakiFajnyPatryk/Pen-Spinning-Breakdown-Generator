cdd1125fab9752fb6140e99f089e33f211327ca3
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-01-17 20:19:37 +0100

    Fixed Scissor's Spin

--- data/tricks/scissors_spin.sql
+++ data/tricks/scissors_spin.sql
@@ -1,10 +1,10 @@
 --################################################## SCISSOR'S SPIN ##################################################--
 
-INSERT INTO trick (name, acronym, normality) VALUES ('Scissor''s Sonic', NULL, 'Normal');
+INSERT INTO trick (name, acronym, normality) VALUES ('Scissor''s Spin', NULL, 'Normal');
 
 -------------------------------------------------- Scissor's Spin 0.5 --------------------------------------------------
 
-BEGIN new_variant('Scissor''s Sonic', 'Arounds & Spins', 3); END;
+BEGIN new_variant('Scissor''s Spin', 'Arounds & Spins', 3); END;
 
 BEGIN new_modification('0.5'); END;
 
@@ -20,7 +20,7 @@ BEGIN new_pen_positions('mid-low', 'mid-high', 'Normal'); END;
 
 -------------------------------------------------- Scissor's Spin 1.0 --------------------------------------------------
 
-BEGIN new_variant('Scissor''s Sonic', 'Arounds & Spins', 3); END;
+BEGIN new_variant('Scissor''s Spin', 'Arounds & Spins', 3); END;
 
 BEGIN new_modification('1.0'); END;
 
@@ -36,7 +36,7 @@ BEGIN new_pen_positions('mid-low', 'mid-high', 'Normal'); END;
 
 -------------------------------------------------- Scissor's Spin 1.5 --------------------------------------------------
 
-BEGIN new_variant('Scissor''s Sonic', 'Arounds & Spins', 3); END;
+BEGIN new_variant('Scissor''s Spin', 'Arounds & Spins', 3); END;
 
 BEGIN new_modification('1.5'); END;
 
@@ -52,7 +52,7 @@ BEGIN new_pen_positions('mid-low', 'mid-high', 'Normal'); END;
 
 -------------------------------------------------- Scissor's Spin 2.0 --------------------------------------------------
 
-BEGIN new_variant('Scissor''s Sonic', 'Arounds & Spins', 3); END;
+BEGIN new_variant('Scissor''s Spin', 'Arounds & Spins', 3); END;
 
 BEGIN new_modification('2.0'); END;
 
